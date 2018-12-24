bfb64c3b85173421aedd91dc6781b3a4b04cbf9a
Evgenii Babkin <evgeniy.babkin94@gmail.com>
2018-12-24 22:14:24 +0300

    First commit

---
+++ src/FirstServlet.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FirstServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+        String name = request.getParameter("login");
+        String surname = request.getParameter("password");
+        PrintWriter pw = response.getWriter();
+
+
+
+       pw.println(name );
+
+
+
+    }
+}
---
+++ web/firstJsp.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: evgenii
+  Date: 23.12.18
+  Time: 15:37
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>First JSP</title>
+</head>
+<body>
+    <h1>Testing JSP</h1>
+    <p>
+        <%= new java.util.Date() %>
+    </p>
+</body>
+</html>
